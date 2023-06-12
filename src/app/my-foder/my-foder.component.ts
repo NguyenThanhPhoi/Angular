a18abf5e7d6ca93ca6d057136bc7bd9528d6e9d5
NguyenThanhPhoi <nguyenthanhphoi01@gmail.com>
2023-06-12 20:49:46 +0700

    init

---
+++ src/app/my-foder/my-foder.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-foder',
+  templateUrl: './my-foder.component.html',
+  styleUrls: ['./my-foder.component.scss']
+}) 
+export class MyFoderComponent {
+  title = 'my-first-applicatons';
+}
